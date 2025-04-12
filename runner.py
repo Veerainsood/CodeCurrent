76726b96e7a3cd62b1b9502fbc1cd6f7bb24807a
Veerain <Veerainsood1@gmail.com>
2025-04-12 13:46:19 +0530

    Fixed Everything...

--- runner.py
+++ runner.py
@@ -111,7 +111,7 @@ def save_json(data, path):
     with open(path, 'w') as f:
         json.dump(data, f, indent=4)
 
-Load the caller-callee pairs JSON file.
+# Load the caller-callee pairs JSON file.
 caller_callee_pairs = load_json("interlangCpp.json")
     
 # Load the function_calls_combined JSON file.
